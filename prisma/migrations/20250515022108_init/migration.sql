369de6fa403670567e29afbeaff0b90d6761a97b
ToDoList <christian123star@gmail.com>
2025-05-15 09:25:24 +0700

    first commit

--- prisma/migrations/20250515022108_init/migration.sql
+++ prisma/migrations/20250515022108_init/migration.sql
@@ -27,7 +27,7 @@ CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "passwrod" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "isDeleted" BOOLEAN NOT NULL DEFAULT false,
